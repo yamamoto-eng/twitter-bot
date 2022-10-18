49c1bf68f464bfee4b7e23213eeb9d35cd2e0abe
shunsuke-yamamoto <78201056+ys-eng@users.noreply.github.com>
2022-10-18 23:12:19 +0900

    update: log

--- services/like/deleteLike.py
+++ services/like/deleteLike.py
@@ -17,7 +17,9 @@ def deleteLike():
     my_id = client.get_me().data.id
     execute_num = 1
     liked_tweets = client.get_liked_tweets(my_id).data
-    print("\n", now.strftime("%H時%M分"), inspect.currentframe().f_code.co_name, "\n")
+    print("\n--------------------------------------------------")
+    print(now.strftime("%H時%M分"), inspect.currentframe().f_code.co_name)
+    print("--------------------------------------------------\n")
 
     for liked_tweet in liked_tweets:
         if count < execute_num:
