c18957f720e7dbc54aab45ed1feac426d5466b9e
shunsuke-yamamoto <78201056+ys-eng@users.noreply.github.com>
2022-10-19 22:42:16 +0900

    add: mypy

--- execute.py
+++ execute.py
@@ -4,9 +4,20 @@ sys.dont_write_bytecode = True
 
 import schedule
 from time import sleep
+import os
+import datetime
 from services.like.addLike import addLike
 from services.like.deleteLike import deleteLike
 from services.retweet.deleteRetweet import deleteRetweet
+
+
+print("\n--------------------------------------------------")
+print(
+    "start",
+    datetime.datetime.now().strftime("%H時%M分"),
+    os.path.basename(__file__),
+)
+print("--------------------------------------------------\n")
 
 
 def execute():
@@ -15,7 +26,7 @@ def execute():
     deleteRetweet()
 
 
-schedule.every(1800).seconds.do(execute)
+schedule.every(60).minutes.do(execute)
 
 
 while True:
